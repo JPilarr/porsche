2516edce04c7eec9fe6d020e610d7abb03bf5855
mesmerlord <76161333+mesmerlord@users.noreply.github.com>
2021-12-06 11:16:10 +0100

    initial serverless config

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,39 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+import boto3
+import json
 
 app = Flask(__name__)
 
+#Placeholder function for auth flow later    
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    parsed = request.json
+    client = boto3.client('cognito-idp')
+    response = client.initiate_auth(
+        UserPoolId='xxxxxxxxx',
+        ClientId='xxxxxxxxxxxxxx',
+        AuthFlow='USER_PASSWORD_AUTH',
+        AuthParameters={
+            'USERNAME': parsed['username'],
+            'PASSWORD': parsed['password']
+        }
+    )
+    return jsonify(message='Hello from signup!')
+
+#Placeholder function for auth flow later    
+@app.route("/login", methods=["POST"])
+def login():
+    client = boto3.client('cognito-idp')
+    response = client.initiate_auth(
+        UserPoolId='xxxxxxxxx',
+        ClientId='xxxxxxxxxxxxxx',
+        AuthFlow='USER_PASSWORD_AUTH',
+        AuthParameters={
+            'USERNAME': 'xxxxxx',
+            'PASSWORD': 'xxxxxx'
+        }
+    )
+    return response
 
 @app.route("/")
 def hello_from_root():
